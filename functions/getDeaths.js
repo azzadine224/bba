4c5debd1435a67f29e95ad8cec3cdd5b98867cb0
azzadine224 <88667140+azzadine224@users.noreply.github.com>
2021-12-30 14:26:46 +0100

    Update getDeaths.js

--- functions/getDeaths.js
+++ functions/getDeaths.js
@@ -7,7 +7,7 @@ exports.handler = async () => {
      const deaths = await axois.get(url)
      const deathsInfo =  deaths.data
      const headers = {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://61cdb0a3e898a100083ecf30--blissful-tereshkova-46d989.netlify.app/,*',
         'Access-Control-Allow-headers': 'origin, X-Requested-With, content-type, accept',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       };
